0aef429fc58ae865fb5d0a019266817a3cd0fab8
ResearchScientist <8367366+ResearchScientist@users.noreply.github.com>
2020-12-28 14:02:07 -0800

    change import settings

--- src/deleteTask.js
+++ src/deleteTask.js
@@ -1,7 +1,8 @@
-import cs from "./addTask";
+import p from "./index";
 
 const cs = document.getElementById('commentsection');
 cs.addEventListener("click", function() {
   console.log('changing colour');
   cs.style.backgroundColor = 'orange';
+  p.style.backgroundColor = 'orange';
 });
