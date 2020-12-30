499ae1c73279fd94a0a76a2342b46e26bbd3050e
ResearchScientist <8367366+ResearchScientist@users.noreply.github.com>
2020-12-30 10:45:34 -0800

    make button

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import {showThings} from './show.js';
 import {kittens,lions} from './cats.js';
 import {toys,food} from './things.js';
+import {makeButton} from './button.js';
 
 console.log(showThings(lions));
+
+const button = makeButton("I'm cute as a button");
+document.body.appendChild(button);
