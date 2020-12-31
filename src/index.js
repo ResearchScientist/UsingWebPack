5a3c73571a1274b5e14a942a2c7daf3733c4aea7
ResearchScientist <8367366+ResearchScientist@users.noreply.github.com>
2020-12-31 15:31:06 -0800

    add footer on clik

--- src/index.js
+++ src/index.js
@@ -7,3 +7,14 @@ console.log(showThings(lions));
 
 const button = makeButton("I'm cute as a button");
 document.body.appendChild(button);
+
+
+//button.addEventListener("click", e => {
+//  import("./footer").then(footerModule => {
+//    document.body.appendChild(footerModule.footer);
+//  });
+//});
+
+button.addEventListener("click",e => {
+  document.body.appendChild(footer);
+});
