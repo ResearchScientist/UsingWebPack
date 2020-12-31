4fedc21a74bb6778d84d8434344c6b21dccbfe49
ResearchScientist <8367366+ResearchScientist@users.noreply.github.com>
2020-12-31 15:45:02 -0800

    make footer

---
+++ src/footer.js
@@ -0,0 +1,7 @@
+const makeFooter = () => {
+
+  const footer = document.createElement("footer");
+  return footer;
+};
+
+export {makeFooter,footer};
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import {showThings} from './show.js';
 import {kittens,lions} from './cats.js';
 import {toys,food} from './things.js';
 import {makeButton} from './button.js';
+import {footer} from './footer.js';
 
 console.log(showThings(lions));
 
