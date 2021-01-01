0b3961b3672cf071d59aebcc0fc05218f07b321e
ResearchScientist <8367366+ResearchScientist@users.noreply.github.com>
2020-12-31 17:27:33 -0800

    test: import footer with promise

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import {showThings} from './show.js';
 import {kittens,lions} from './cats.js';
 import {toys,food} from './things.js';
 import {makeButton} from './button.js';
-import {footer} from './footer.js';
+// import {footer} from './footer.js';
 
 console.log(showThings(lions));
 
@@ -10,12 +10,12 @@ const button = makeButton("I'm cute as a button");
 document.body.appendChild(button);
 
 
-//button.addEventListener("click", e => {
-//  import("./footer").then(footerModule => {
-//    document.body.appendChild(footerModule.footer);
-//  });
-//});
-
-button.addEventListener("click",e => {
-  document.body.appendChild(footer);
+button.addEventListener("click", e => {
+  import("./footer").then(footerModule => {
+    document.body.appendChild(footerModule.footer);
+  });
 });
+
+//button.addEventListener("click",e => {
+//  document.body.appendChild(footer);
+//});
